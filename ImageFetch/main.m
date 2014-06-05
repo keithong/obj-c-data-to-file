ee76fd6c0392c09d1e3fc4566dcac382531b622e
Keith Samson <k.samson@klab.com>
2014-06-05 10:43:42 +0800

    ImageFetch's initial commit

---
+++ ImageFetch/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by Keith Samson on 6/5/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Writing an NSData object to a file");
+        
+    }
+    return 0;
+}
+
