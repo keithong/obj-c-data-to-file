d76b12a9f0b94397ccb24f3e7a4faeb8d0237b51
Keith Samson <k.samson@klab.com>
2014-06-05 10:51:44 +0800

    Written an image using NSData

--- ImageFetch/main.m
+++ ImageFetch/main.m
@@ -13,9 +13,31 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Writing an NSData object to a file");
         
+        NSURL *url = [NSURL URLWithString:@"http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg"];
+        
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        NSError *error = nil;
+        NSData *data = [NSURLConnection
+                        sendSynchronousRequest:request
+                        returningResponse:NULL
+                        error:&error];
+        if(!data){
+            NSLog(@"Fetch failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"The file is %lu",[data length]);
+        
+        BOOL written = [data writeToFile:@"/tmp/cat.png"
+                                 options:0
+                                   error:&error];
+        
+        if(!written){
+            NSLog(@"Write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        NSLog(@"Success!");
     }
     return 0;
 }
